3bd28dbb6b52f7324c55aab9719bb29e7961c07e
Diego Sosa <diego.stt@live.com>
2016-06-18 22:47:17 -0600

    Wit

--- fbMessengerBot/handleWitMessages.js
+++ fbMessengerBot/handleWitMessages.js
@@ -1,4 +1,14 @@
+var sendMessage = require('./fbMessage/sendMessage');
+var fbMessage = require('./fbMessage/fbMessage');
+
 module.exports = function (config) {
   var senderId = config.senderId;
-  var commands = config.command.split(' ');
+  var message = config.messageText.split(' ');
+
+  var textReply = new fbMessage
+      .PlainText("SenderId: " + senderId + " Message JSON: " + JSON.stringify(message))
+      .compose();
+
+  sendMessage(senderId, textReply);
+
 }
