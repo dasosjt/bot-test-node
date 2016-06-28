5e53eb07d57d3070c17a3427a3cb5cd0067eea28
Diego Sosa <diego.stt@live.com>
2016-06-28 15:09:34 -0600

    mixco action

--- fbMessengerBot/handleWitMessages.js
+++ fbMessengerBot/handleWitMessages.js
@@ -31,6 +31,19 @@ const findOrCreateSession = (senderId) => {
   return sessionId;
 };
 
+//Search for entities
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
 // Our bot actions
 const actions = {
   say(sessionId, context, message, cb) {
@@ -67,6 +80,11 @@ const actions = {
     cb();
   },
   merge(sessionId, context, entities, message, cb) {
+    // Retrieve the location entity and store it into a context field
+    /*const loc = firstEntityValue(entities, 'location');
+    if (loc) {
+      context.loc = loc;
+    }*/
     cb(context);
   },
   error(sessionId, context, error) {
@@ -74,19 +92,12 @@ const actions = {
   },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
-
-  ['municipios'](sessionId, context, cb) {
+  /*['fetch-weather'](sessionId, context, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
-    context.E = 'sunny';
+    context.forecast = 'sunny';
     cb(context);
-  },
-  ['municipios1'](sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
-    context.E = 'sunny';
-    cb(context);
-  },
+  },*/
 
 };
 
