9e440213e238ba7cfa501808df68601488521038
Diego Sosa <diego.stt@live.com>
2016-07-02 21:19:56 -0600

    Wit V1

--- fbMessengerBot/handleWitMessages.js
+++ fbMessengerBot/handleWitMessages.js
@@ -57,7 +57,7 @@ const actions = {
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
       // We return a promise to let our bot know when we're done sending
-      return sendMessageWit(recipientId, message)
+      return sendMessage(recipientId, message)
       .then(() => null)
       .catch((err) => {
         console.error(
