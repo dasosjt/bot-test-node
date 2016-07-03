1fbcf20df70e19cfbf56b8633afdb545e095a22f
Diego Sosa <diego.stt@live.com>
2016-07-03 17:04:30 -0600

    witv1

--- fbMessengerBot/handleWitMessages.js
+++ fbMessengerBot/handleWitMessages.js
@@ -101,6 +101,12 @@ const actions = {
 const wit = new Wit({
   accessToken: WIT_TOKEN,
   actions,
+  myAction({sessionId, context, text, entities}) {
+      console.log(`Session ${sessionId} received ${text}`);
+      console.log(`The current context is ${JSON.stringify(context)}`);
+      console.log(`Wit extracted ${JSON.stringify(entities)}`);
+      return Promise.resolve(context);
+  },
   logger: new log.Logger(log.INFO)
 });
 
